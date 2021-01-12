fc440457ac4f58f4ecc2f97beafdec62ab85bc27
Biggs Thorarensen <biggs@devillion.com>
2021-01-12 12:06:24 +0000

    constraining header width

--- tailwind.config.js
+++ tailwind.config.js
@@ -328,6 +328,9 @@ module.exports = {
       '4xl': '56rem',
       '5xl': '64rem',
       '6xl': '72rem',
+      '7xl': '80rem',
+      '8xl': '88rem',
+      '9xl': '96rem',
       full: '100%',
     },
     minHeight: {
